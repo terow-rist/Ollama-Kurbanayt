481689c95f86f0b89f557af46476513fe1fa1eb7
missayev <feeerhd.342@gmail.com>
2025-02-13 03:29:50 +0500

    feat: history popup chat logic

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ from backend.routes.llms import router as return_ollama
 from backend.routes.auth import router as router_auth
 from fastapi.staticfiles import StaticFiles
 
+from backend.routes.history import router as router_history
 
 
 app = FastAPI()
@@ -14,5 +15,8 @@ app.include_router(return_html)
 app.include_router(return_ollama)
 
 app.include_router(router_auth)
+
+app.include_router(router_history)
+
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
