d585f5aef759a541001f1a723dc6555f840d5006
ALHNk <88340494+ALHNk@users.noreply.github.com>
2025-02-13 01:57:56 +0500

    chromadb add and new-tab

--- backend/routes/llms.py
+++ backend/routes/llms.py
@@ -6,11 +6,13 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from LLM import ollama
-
 router = APIRouter()
 
 class QueryRequest(BaseModel):
     query: str
+
+class ChromaData(BaseModel):
+    content: str 
 
 @router.get("/oll")
 def func():
@@ -24,5 +26,16 @@ async def ask_ollama(request: QueryRequest):
     
     answer = await ollama.ollama_answer(query_text)
     print(answer)
-    
+    latest_answer = answer
     return {"answer": answer}
+
+@router.get("/chromadata")
+async def show_all_data():
+    documents = await ollama.get_from_collection()
+    return {"documents":documents}
+
+@router.post("/chromadata")
+async def add_data(data: ChromaData):
+    is_added = await ollama.add_to_collection(data.content)
+    print(is_added)
+    return{"status": is_added}
